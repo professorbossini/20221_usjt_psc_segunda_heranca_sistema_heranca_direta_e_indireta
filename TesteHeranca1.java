756d424c1a87b8a9c5c5fab8bd1c24eb22ec85eb
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-25 20:30:48 -0300

    Tentando acessar nome e matricula com variaveis Pessoa e Professor

--- TesteHeranca1.java
+++ TesteHeranca1.java
@@ -1,5 +1,14 @@
 public class TesteHeranca1 {
     public static void main(String[] args) {
-        
+        //Professor tem nome?
+        Professor prof = new Professor();
+        prof.nome = "Rodrigo";
+        prof.matricula = 123456;
+
+        //Pessoa tem matricula?
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Jaqueline";
+        pessoa.matricula = 123456;
+
     }
 }
